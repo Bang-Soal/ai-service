b8810b95e29f8a56d969923273314ef7648b311b
Umar Izzuddin <izzuddinumar13@gmail.com>
2024-08-14 10:36:03 +0700

    feat: change to using gpt 4o on paraphrase question

--- service/paraphrase_question.py
+++ service/paraphrase_question.py
@@ -89,7 +89,7 @@ class paraphrase_question() :
     Dengan demikian jawaban yang benar adalah D. Neto tara
     """
 
-            model = 'gpt-4-1106-preview'
+            model = 'gpt-4o'
 
             messages = [
                             {

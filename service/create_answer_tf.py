05be139e78bba411666dc80b62cf0b592c304994
Han-Zalwi <hanzalwi@gmail.com>
2024-10-09 22:18:12 +0700

    update true false generate answer

--- service/create_answer_tf.py
+++ service/create_answer_tf.py
@@ -9,31 +9,18 @@ class create_answer_tf() :
          organization=env_data.org_key(),
          api_key=env_data.api_key()
         )
-        task = """
-Anda adalah sebuah sistem pembuat soal berjenis TRUE-FALSE dari sebuah argumen yang diberikan dan penjelasan jawaban dengan tingkat kesulitan yang lebih tinggi dibandingkan soal yang diberikan saat ini. Bahasa yang digunakan antara bahasa indonesia atau bahasa inggris menyesuaikan input yang diberikan. Penjelasan wajib menggunakan bahasa indonesia karena target penggunanya adalah siswa yang berasal dari indonesia
-URUTAN INPUT : [QUESTION], [TOTAL_QUESTION]
-URUTAN OUTPUT : [NEW_QUESTION], [NEW_ANSWER], [NEW_CHOICE]
+        task = f"""Anda adalah sebuah sistem penjawab soal berbahasa indonesia dengan jenis benar salah
+URUTAN INPUT : [QUESTION], [CHOICE], [DESCRIPTION]
+URUTAN OUTPUT : [FINAL_ANSWER]
 
 Soal-soal ini dapat meliputi soal penalaran umum, matematika, bacaan bahasa indonesia, atau bacaan bahasa inggris
-Soal-soal yang anda buat adalah soal BARU, anda tidak bisa merefer ke soal sebelumnya. Anda perlu menjelaskan kembali apa yang diperlukan
-Apabila soal tersebut bertipe sebuah soal yang mengandung teks bahasa indonesia ataupun teks bahasa inggris, anda perlu membuat teks baru sehingga soal yang dibuat tidak merefer ke teks sebelumnya
-Apabila soal dan jawaban menggunakan bahasa inggris, anda wajib tetap menggunakan bahasa inggris untuk membuat soal dan jawaban baru. tujuan anda bukan untuk mengartikan atau mengubah soal dan jawabannya ke bahasa indonesia
 Anda akan diberikan sebuah pertanyaan dengan label [QUESTION]
-Anda akan diberikan sebuah total argumentasi TRUE-FALSE yang perlu dibuat [TOTAL_QUESTION]
-Anda ditugaskan untuk membuat soal baru tersebut dengan memahami kedua aspek yang telah diberikan : [QUESTION] dan [TOTAL_QUESTION] untuk menghasilkan soal baru yang serupa, tetapi dengan tingkat kesulitan yang lebih tinggi [NEW_QUESTION], pilihan argumen TRUE-FALSE berdasarkan soal baru [NEW_CHOICE], dan pembahasan jawabannya [NEW_ANSWER]
-Format dari [NEW_CHOICE] adalah "[Urutan Abjad]. Argumen - TRUE/FALSE"
+Pilihan jawaban dengan label [CHOICE] yang berisikan pilihan jawaban
+Pembahasan jawaban dengan label [DESCRIPTION] dengan label TRUE/FALSE
 
-Langkah pembuatan :
-Hal yang pertama dibuat adalah pertanyaan yang akan dijadikan sebagai [NEW_QUESTION]
-Anda akan mendeteksi terlebih dahulu bahasa apa yang digunakan pada [NEW_QUESTION], yaitu bahasa indonesia atau bahasa inggris
-Apabila pada soal mengandung sebuah bacaan teks bahasa indonesia atau teks bahasa inggris, yaitu sebuah teks bacaan yang memuat minimal terdiri dari 2 paragraf atau minimal 200 kata, anda perlu membuat ulang sebuah teks dengan minimal 3 paragraf atau minimal 300 kata (MINIMUM 3 PARAGRAPH OR 250 TEXT) pada [NEW_QUESTION] yang masih relevan pada topik bacaan di [QUESTION] . Dilarang untuk memberikan bacaan yang panjang teksnya terdiri dari 1 paragraf saja atau dibawah 200 kata pada [NEW_QUESTION] untuk kasus ini 
-Apabila [QUESTION] menggunakan bahasa inggris, anda tetap harus menggunakan bahasa inggris untuk membuat [NEW_QUESTION] 
-Setelah membuat paragara [NEW_QUESTION], paragraf [NEW_QUESTION] wajib ditambah dengan kalimat ini "Pernyataan-pernyataan di bawah ini yang tepat adalah ..."
-Setelah itu dibuatlah argumen-argumen TRUE/FALSE pada [NEW_CHOICE] berdasarkan jumlah argumen yang diminta [TOTAL_QUESTION]
-Setelah itu dibuatlah pembahasan yang terperinci dari [NEW_QUESTION] berdasarkan [TOTAL_QUESTION] untuk membuat [NEW_ANSWER]
-Untuk soal matematika, [NEW_ANSWER] diharapkan juga menampilkan langkah-langkah sampai jawaban akhir
-[NEW_ANSWER] menjelaskan secara rinci mengenai argumen-argumen yang ada pada [NEW_CHOICE] mengapa argumen tersebut dapat bernilai TRUE atau FALSE
-Format dari [NEW_ANSWER] untuk setiap argumen-argumen yang ada pada [NEW_CHOICE] adalah 
+Anda ditugaskan untuk membuat jawaban berdasarkan soal yang telah diberikan dengan cara memahami [QUESTION], [CHOICE], dan [DESCRIPTION]
+[FINAL_ANSWER] menjelaskan secara rinci mengenai argumen-argumen yang ada pada [CHOICE] mengapa argumen tersebut dapat bernilai TRUE atau FALSE
+Format dari [FINAL_ANSWER] untuk setiap argumen-argumen yang ada pada [CHOICE] adalah 
 Argumen - TRUE/FALSE
 Explanation : [penjelasan argumen] 
 
@@ -41,11 +28,17 @@ contoh :
 Rising ocean temperatures will reduce the frequency of hurricanes and typhoons. - FALSE
 Explanation: Naiknya suhu laut sebenarnya meningkatkan kekerapan dan intensitas badai seperti angin topan dan hurricane karena menyediakan lebih banyak energi dan uap air.
 
-NOTE:
-Apabila soal bertipe matematika atau perhitungan, diharapkan pada bagian pemahaman soal anda dapat menjabarkan secara detail langkah-langkah untuk mengerjakan soal yang tetera bersamaan dengan hasil perhitungannya pada [NEW_QUESTION], tidak perlu menjelaskan satu-satu jawaban yang ada pada [NEW_CHOICE]
-Apabila soal berbahasa indonesia, wajib menggunakan bahasa indonesia untuk membuat [NEW_QUESTION] dan [NEW_CHOICE], tetapi untuk penjelasan yang ada pada [NEW_ANSWER] wajib menggunakan bahasa indonesia. Namun, untuk urutan keluaran tetap wajib seperti urutan ini : [NEW_QUESTION], [NEW_ANSWER], [NEW_CHOICE]
-Apabila soal berbahasa inggris, wajib menggunakan bahasa inggris untuk membuat [NEW_QUESTION] dan [NEW_CHOICE], tetapi untuk penjelasan yang ada pada [NEW_ANSWER] wajib menggunakan bahasa indonesia. Namun, untuk urutan keluaran tetap wajib seperti urutan ini : [NEW_QUESTION], [NEW_ANSWER], [NEW_CHOICE]
-Sebelum output dikeluarkan, mohon pastikan kembali urutan output adalah [NEW_QUESTION] dilanjutkan dengan [NEW_ANSWER] dan ditutup dengan [NEW_CHOICE]
+
+Contoh Output : 
+[FINAL_ANSWER]:
+1. Kantong plastik dapat bertahan hingga 500 tahun di lingkungan - TRUE
+Explanation: Kantong plastik terbuat dari polimer yang sangat tahan lama dan tidak mudah terurai secara biologis, sehingga memungkinkan mereka bertahan di lingkungan hingga berabad-abad.
+
+2. Penggunaan kembali kantong plastik tidak mempengaruhi kesehatan manusia - FALSE
+Explanation: Penggunaan kembali kantong plastik, terutama yang sudah tua dan mulai terurai, dapat melepaskan zat kimia seperti Bisphenol A (BPA), yang diketahui memberikan efek negatif pada kesehatan, termasuk pengaruh pada hormon dan potensi gangguan kesehatan serius lainnya.
+
+3. Kebijakan larangan penggunaan kantong plastik telah efektif mengurangi polusi plastik di seluruh dunia - FALSE
+Explanation: Meskipun telah ada upaya dan kebijakan larangan penggunaan kantong plastik di beberapa negara, penggunaannya masih tinggi di beberapa bagian dunia dan efektivitas kebijakan ini dalam mengurangi polusi secara global masih terbatas.
 """
 
         complation = client.chat.completions.create(
